921133f7d29ea41bd0910df48381ea92f8c8a902
John Smith <ubuntu@ip-10-252-34-89.us-west-2.compute.internal>
2013-07-12 19:04:55 +0000

    initial checkin

---
+++ apt-gets.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+echo sudo apt-get update
+sudo apt-get update
+echo sudo apt-get -y upgrade autoremove
+sudo apt-get -y upgrade autoremove
+echo sudo apt-get -y install mysql
+sudo apt-get -y install mysql
+echo sudo apt-get -y install git-core
+sudo apt-get -y install git-core
+echo sudo apt-get install -y rlwrap
+sudo apt-get install -y rlwrap
+# Install emacs24
+# https://launchpad.net/~cassou/+archive/emacs
+echo sudo apt-add-repository -y ppa:cassou/emacs
+sudo apt-add-repository -y ppa:cassou/emacs
+echo sudo apt-get install -y emacs24 emacs24-el emacs24-common-non-dfsg
+sudo apt-get install -y emacs24 emacs24-el emacs24-common-non-dfsg
+echo sudo apt-get install -y python-software-properties python g++ make
+sudo apt-get install -y python-software-properties python g++ make
+echo sudo add-apt-repository ppa:chris-lea/node.js
+sudo add-apt-repository -y ppa:chris-lea/node.js
+echo sudo apt-get install -y nodejs
+sudo apt-get install -y nodejs
---
+++ doit.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+sudo ./git_stuff.sh
+sudo ./apt-gets.sh
+sudo ./dotfiles_setup.sh
+sudo ./node_setup.sh
+
