aa33ebbab11091135008fb8f94a84c5c789ebbbe
John Smith <willey@selby.com>
2013-07-12 20:13:25 +0000

    add mysql sesrver

--- apt-gets.sh
+++ apt-gets.sh
@@ -4,8 +4,10 @@ echo sudo apt-get update
 sudo apt-get update
 echo sudo apt-get -y upgrade autoremove
 sudo apt-get -y upgrade autoremove
-echo sudo apt-get -y install mysql
-sudo apt-get -y install mysql
+echo sudo apt-get -y install mysql-server
+sudo apt-get -y install mysql-server
+echo sudo apt-get install mysql-client-core-5.5 mysql-client-5.5
+sudo apt-get -y install mysql-client-core-5.5 mysql-client-5.5
 echo sudo apt-get -y install git-core
 sudo apt-get -y install git-core
 echo sudo apt-get install -y rlwrap
